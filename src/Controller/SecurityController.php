50d13ebf6ff5e8f8cc2508510f59779e6aefccf0
Dariusz <37488679+Volmarg@users.noreply.github.com>
2019-02-03 13:56:33 +0100

    1.01 blocked login while user is already logged in, added css from symfonycast to loginfrom

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,13 +12,21 @@ class SecurityController extends AbstractController {
      * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response {
+        $referer_url = (array_key_exists('HTTP_REFERER', $_SERVER) ? $_SERVER['HTTP_REFERER'] : null);
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            if (!is_null($referer_url)) {
+                return $this->redirect($referer_url);
+            }
+            return $this->redirect('/');
+        } else {
+            return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        }
     }
 
 }
